6e519505cee2687c1d6dac785f967d499f4b3762
mhollow56 <mikeholloway7023@protonmail.com>
2022-10-11 18:01:56 -0500

    changes

---
+++ src/app/bookshelf/book-list/book-list.component.ts
@@ -0,0 +1,34 @@
+import { Component, OnInit,  } from '@angular/core';
+import { Book } from 'src/app/shared/book/book.model';
+@Component({
+  selector: 'app-book-list',
+  templateUrl: './book-list.component.html',
+  styleUrls: ['./book-list.component.css']
+})
+export class BookListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+  myBooks: Book[] = [
+    new Book(
+        'Book of Testing',
+        'Will Wilder',
+        'Mystery',
+        'https://source.unsplash.com/50x50/?mystery,book'
+    ),
+    new Book(
+      'Book of More Testing',
+      'John Smith',
+      'Drama',
+      'https://source.unsplash.com/50x50/?mystery,book'
+  ),
+  new Book(
+    'Book of All Testing',
+    'Mike Holloway',
+    'Documentary',
+    'https://source.unsplash.com/50x50/?mystery,book'
+)
+];
+}
---
+++ src/app/library/book-results/book-results.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+import { Book } from 'src/app/shared/book/book.model';
+@Component({
+  selector: 'app-book-results',
+  templateUrl: './book-results.component.html',
+  styleUrls: ['./book-results.component.css']
+})
+export class BookResultsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+  allBooks: Book[] = [
+    new Book(
+        'Book of Testing',
+        'Will Wilder',
+        'Mystery',
+        'https://source.unsplash.com/50x50/?mystery,book'
+    ),
+    // . . .
+];
+}
